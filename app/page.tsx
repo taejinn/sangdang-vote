3bcf1c258f50085d8de8bd4151b5cc1461b2a755
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2024-11-19 06:38:59 +0900

    홈 화면 페이지이동 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+'use client';
+
 import styles from "./page.module.css";
 import { MdHowToVote } from "react-icons/md";
 import { MdSettings } from "react-icons/md";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+    const router = useRouter();
+
   return (
     <>
         <div className={styles.container}>
@@ -14,7 +19,7 @@ export default function Home() {
                 아래 유형을 선택해주세요
             </div>
             <div className={styles.box}>
-                <div className={styles.select} id={styles.selectGradient}>
+                <div onClick={() => router.push('/vote')} className={styles.select} id={styles.selectGradient}>
                     <MdHowToVote size={40}/>
                     <div className={styles.name}>
                         투표 참여
@@ -22,7 +27,7 @@ export default function Home() {
                 </div>
                 <div className={styles.select}>
                     <MdSettings size={40}/>
-                    <div className={styles.name}>
+                    <div onClick={() => router.push('/admin')} className={styles.name}>
                         투표 관리
                     </div>
                 </div>
